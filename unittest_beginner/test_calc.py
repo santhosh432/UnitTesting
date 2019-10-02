b07638cfd857845e688b3cf8fa73b21ffd6fe7ff
santhosh <santhoshcusat17@gmail.com>
2019-10-02 11:16:25 +0530

    Unit test: How to run unittest

--- unittest_beginner/test_calc.py
+++ unittest_beginner/test_calc.py
@@ -5,8 +5,20 @@ import calc
 
 
 class TestCaseCalc(unittest.TestCase):
+    def setUp(self):
+        print('This instance method is for SetUp')
+
     def test_add(self):
         self.assertEqual(calc.add(2,3), 5)
 
+    def tearDown(self):
+        print('This instance method is for teardown')
+
 if __name__ == "__main__":
     unittest.main()
+
+
+'''
+Another way to run unit test is
+python3 -m unittest test_cal.py
+'''
